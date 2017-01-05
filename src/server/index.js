e4edfb933aa36f647fff6af41e55b9d763fa447c
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-01-05 02:20:44 -0800

    fix lint

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable */
+// Babel itself need a var declaration, so we disable eslint for this file.
+
 var babel = require('babel-core/register')({
 	presets: ['es2015', 'stage-2', 'react']
 });

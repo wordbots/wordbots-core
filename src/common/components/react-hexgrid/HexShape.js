7b6f12c3b5d63c9b2d241951e810f9b43a5e2255
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-01-30 16:44:54 -0800

    oops

--- src/common/components/react-hexgrid/HexShape.js
+++ src/common/components/react-hexgrid/HexShape.js
@@ -57,31 +57,43 @@ class HexShape extends React.Component {
   getPieceStats() {
     if (this.props.piece) {
       if (this.props.pieceStats.attack) {
-        <g>
-          <circle style={{
-            fill: '#81C784'
-          }} cx="3" cy="2" r="2" />
-          <text x="3" y="3" textAnchor="middle" style={{
-            fontFamily: 'Luckiest Guy',
-            fontSize: '0.19em',
-            fill: '#FFFFFF',
-            fillOpacity: 1
-          }}>{this.props.pieceStats.health}</text>
-        </g>
+        return (
+          <g>
+            <circle style={{
+              fill: '#E57373'
+            }} cx="-3" cy="2" r="2" />
+            <text x="-3" y="3" textAnchor="middle" style={{
+              fontFamily: 'Luckiest Guy',
+              fontSize: '0.19em',
+              fill: '#FFFFFF',
+              fillOpacity: 1
+            }}>{this.props.pieceStats.attack}</text>
+            <circle style={{
+              fill: '#81C784'
+            }} cx="3" cy="2" r="2" />
+            <text x="3" y="3" textAnchor="middle" style={{
+              fontFamily: 'Luckiest Guy',
+              fontSize: '0.19em',
+              fill: '#FFFFFF',
+              fillOpacity: 1
+            }}>{this.props.pieceStats.health}</text>
+          </g>
+        )
+      } else {
+        return (
+          <g>
+            <circle style={{
+              fill: '#81C784'
+            }} cx="3" cy="2" r="2" />
+            <text x="3" y="3" textAnchor="middle" style={{
+              fontFamily: 'Luckiest Guy',
+              fontSize: '0.19em',
+              fill: '#FFFFFF',
+              fillOpacity: 1
+            }}>{this.props.pieceStats.health}</text>
+          </g>
+        )
       }
-      return (
-        <g>
-          <circle style={{
-            fill: '#81C784'
-          }} cx="3" cy="2" r="2" />
-          <text x="3" y="3" textAnchor="middle" style={{
-            fontFamily: 'Luckiest Guy',
-            fontSize: '0.19em',
-            fill: '#FFFFFF',
-            fillOpacity: 1
-          }}>{this.props.pieceStats.health}</text>
-        </g>
-      )
     } else {
       return null;
     }

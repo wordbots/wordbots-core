3762d30dafac8e54de183c67e314ac840956953c
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2017-02-07 17:54:38 -0800

    reset cards to previous values

--- src/common/store/cards.js
+++ src/common/store/cards.js
@@ -24,7 +24,6 @@ export const orangeCoreCard = {
 
 export const attackBotCard = {
   name: 'Attack Bot',
-  text: 'ATTACK!',
   img: 'char',
   cost: 1,
   type: TYPE_ROBOT,
@@ -39,7 +38,6 @@ export const attackBotCard = {
 export const tankBotCard = {
   name: 'Tank Bot',
   img: 'char_weapon',
-  text: 'At the beginning of each turn this creature gets +1 attack',
   cost: 3,
   type: TYPE_ROBOT,
   stats: {
@@ -57,15 +55,6 @@ export const concentrationCard = {
   cost: 1,
   type: TYPE_EVENT
 };
-
-export const concentrationVerboseCard = {
-  name: 'Concentration',
-  text: 'Draw two cards. Draw two cards. Draw two cards. Draw two cards. Draw two cards. Draw two cards. Draw two cards.',
-  command: '(function () { actions["draw"](targets["self"](), 2); })',
-  cost: 1,
-  type: TYPE_EVENT
-};
-
 
 export const superchargeCard = {
   name: 'Supercharge',
@@ -169,13 +158,12 @@ export const firestormCard = {
 export const deck = [
   attackBotCard,
   concentrationCard,
-  concentrationVerboseCard,
-  incinerateCard,
   shockCard,
   superchargeCard,
   tankBotCard,
   firestormCard,
   wisdomCard,
+  incinerateCard,
   discountCard,
   missileStrikeCard,
   rampageCard,

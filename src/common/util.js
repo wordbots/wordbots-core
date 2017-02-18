e118b393903981f42aa2df37df87eb098fceaf61
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-02-18 13:52:55 -0800

    hmm

--- src/common/util.js
+++ src/common/util.js
@@ -77,6 +77,7 @@ export function validPlacementHexes(state, player, type) {
     ]).map(HexUtils.getID);
   }
 
+  console.log(hexes.filter(hex => !allObjectsOnBoard(state)[hex]));
   return hexes.filter(hex => !allObjectsOnBoard(state)[hex]);
 }
 

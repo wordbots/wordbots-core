0215890ad62f4b627dcf66c719ee3438e2062f21
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-02-21 16:27:09 -0800

    Hotfix for attacking + cleanup Board.js and Game.js (#97)
    * Hotfix for attacking + cleanup Board.js and Game.js
    
    * minor
    
    * minor
    
    * More simplification of Board.js

--- src/common/components/game/Board.js
+++ src/common/components/game/Board.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { intersectionBy, mapValues, some } from 'lodash';
+import { forOwn, intersectionBy, mapValues, some } from 'lodash';
 
 import HexGrid from '../react-hexgrid/HexGrid';
 import HexUtils from '../react-hexgrid/HexUtils';
 import { TYPE_ROBOT, TYPE_STRUCTURE } from '../../constants';
 import {
-  getAttribute, hasEffect,
+  getAttribute, hasEffect, ownerOf,
   getAdjacentHexes, validPlacementHexes, validMovementHexes, validAttackHexes
 } from '../../util';
 
@@ -32,55 +32,34 @@ class Board extends Component {
     return (this.props.currentTurn == 'blue' ? this.props.bluePieces : this.props.orangePieces);
   }
 
-  opponentPieces() {
-    return (this.props.currentTurn == 'blue' ? this.props.orangePieces : this.props.bluePieces);
-  }
-
   allPieces() {
     return Object.assign({}, this.props.bluePieces, this.props.orangePieces);
   }
 
-  dummyState() {
-    // util.valid[Placement/Movement/Attack]Hexes() functions require a state object, so we create a dummy one.
-    // TODO find a less gross approach?
-    return {
-      players: {blue: {robotsOnBoard: this.props.bluePieces}, orange: {robotsOnBoard: this.props.orangePieces}}
-    };
-  }
-
   getValidPlacementHexes() {
-    return validPlacementHexes(this.dummyState(), this.props.currentTurn, this.props.playingCardType);
+    return validPlacementHexes(this.dummyGameState, this.props.currentTurn, this.props.playingCardType);
   }
 
   getValidMovementHexes(startHex, speed) {
-    return validMovementHexes(this.dummyState(), startHex, speed);
+    return validMovementHexes(this.dummyGameState, startHex, speed);
   }
 
   getValidAttackHexes(startHex, speed) {
-    return validAttackHexes(this.dummyState(), startHex, speed);
+    return validAttackHexes(this.dummyGameState, this.props.currentTurn, startHex, speed);
   }
 
   updateHexColors() {
     let hexColors = {};
 
-    Object.keys(this.props.bluePieces).forEach((hex) => {
-      if (this.props.currentTurn == 'blue' && this.props.bluePieces[hex].movesLeft > 0) {
-        hexColors[hex] = 'bright_blue';
-      } else {
-        hexColors[hex] = 'blue';
-      }
-    });
+    forOwn(this.allPieces(), (piece, hex) => {
+      const owner = ownerOf(this.dummyGameState, piece).name;
+      const canMove = this.props.currentTurn == owner && piece.movesLeft > 0;
 
-    Object.keys(this.props.orangePieces).forEach((hex) => {
-      if (this.props.currentTurn == 'orange' && this.props.orangePieces[hex].movesLeft > 0) {
-        hexColors[hex] = 'bright_orange';
-      } else {
-        hexColors[hex] = 'orange';
-      }
+      hexColors[hex] = `${canMove ? 'bright_' : ''}${owner}`;
     });
 
     if (this.props.target.choosing) {
-      this.props.target.possibleHexes.forEach((hex) => {
+      this.props.target.possibleHexes.forEach(hex => {
         hexColors[hex]  = 'green';
       });
     } else if (this.props.selectedTile) {
@@ -101,18 +80,15 @@ class Board extends Component {
 
   colorMovementHexes(hex, hexColors, speed) {
     const selectedPiece = this.currentPlayerPieces()[this.props.selectedTile];
-    const existingHexColors = hexColors;
+    const newHexColors = Object.assign({}, hexColors);
 
-    const newHexColors = Object.assign({}, existingHexColors);
+    const movementHexIds = this.getValidMovementHexes(hex, speed).map(HexUtils.getID);
+    const attackHexIds = this.getValidAttackHexes(hex, speed).map(HexUtils.getID);
 
-    this.getValidMovementHexes(hex, speed).forEach(h => {
-      newHexColors[HexUtils.getID(h)] = 'green';
-    });
+    movementHexIds.forEach(h => { newHexColors[h] = 'green'; });
 
     if (!hasEffect(selectedPiece, 'cannotattack')) {
-      this.getValidAttackHexes(hex, speed).forEach((h) => {
-        newHexColors[HexUtils.getID(h)] = 'red';
-      });
+      attackHexIds.forEach(h => { newHexColors[h] = 'red'; });
     }
 
     return newHexColors;
@@ -132,7 +108,7 @@ class Board extends Component {
 
     if (selectedPiece) {
       const selectedHex = HexUtils.IDToHex(this.props.selectedTile);
-      const speed = selectedPiece.stats.speed;
+      const speed = selectedPiece.movesLeft;
 
       const movementHexes = this.getValidMovementHexes(selectedHex, speed).map(HexUtils.getID);
       const attackHexes = this.getValidAttackHexes(selectedHex, speed).map(HexUtils.getID);
@@ -146,7 +122,7 @@ class Board extends Component {
           // Attack destination is not adjacent to current position, so we need an intermediate move action.
           const possibleMoveHexes = intersectionBy(
             getAdjacentHexes(hex),
-            this.getValidMovementSpaces(selectedHex, speed - 1),
+            this.getValidMovementHexes(selectedHex, speed - 1),
             HexUtils.getID
           );
 
@@ -167,6 +143,15 @@ class Board extends Component {
   }
 
   render() {
+    // Many util functions require a game state object, so we create a dummy one with minimal data.
+    // TODO find a less gross approach?
+    this.dummyGameState = {
+      players: {
+        blue: {name: 'blue', robotsOnBoard: this.props.bluePieces},
+        orange: {name: 'orange', robotsOnBoard: this.props.orangePieces}
+      }
+    };
+
     const { grid, config } = this.state;
     const hexColors = this.updateHexColors();
 
@@ -176,10 +161,13 @@ class Board extends Component {
       onMouseLeave: (h, e) => this.onHexHover(h, e)
     };
 
-    const pieces = Object.assign({}, this.currentPlayerPieces(), this.opponentPieces());
+    const pieces = this.allPieces();
     const pieceNames = mapValues(pieces, piece => piece.card.name);
     const pieceImgs = mapValues(pieces, piece => piece.card.img);
-    const pieceStats = mapValues(pieces, (piece) => ({ health: getAttribute(piece, 'health'), attack: getAttribute(piece, 'attack') }));
+    const pieceStats = mapValues(pieces, (piece) => ({
+      health: getAttribute(piece, 'health'),
+      attack: getAttribute(piece, 'attack')
+    }));
 
     return (
       <div>

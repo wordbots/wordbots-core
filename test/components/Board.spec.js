6a271e6ad3e4b9929427569ce57312139cf539eb
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-02-24 13:15:42 -0800

    Test action propagation in Board component

--- test/components/Board.spec.js
+++ test/components/Board.spec.js
@@ -1,11 +1,12 @@
-import { newTurn, setUpBoardState } from '../test_helpers';
-import { createBoard, renderElement } from '../react_helpers';
+import { setUpBoardState, newTurn } from '../test_helpers';
+import { createBoard, renderElement, getComponent } from '../react_helpers';
 import * as actions from '../../src/common/actions/game';
 import { STARTING_PLAYER_HEALTH, GRID_CONFIG } from '../../src/common/constants';
 import gameReducer from '../../src/common/reducers/game';
 import defaultState from '../../src/common/store/defaultState';
 import { attackBotCard } from '../../src/common/store/cards';
 import HexGrid from '../../src/common/components/react-hexgrid/HexGrid';
+import HexUtils from '../../src/common/components/react-hexgrid/HexUtils';
 
 describe('Board component', () => {
   function getHexGridProps(state) {
@@ -35,7 +36,7 @@ describe('Board component', () => {
     );
   });
 
-  it('correctly colors valid movement/attack hexes', () => {
+  describe('[Valid movement/attack hexes]', () => {
     let state = setUpBoardState({
       orange: {
         '1,0,-1': attackBotCard,  // This is the piece that will be selected.
@@ -48,31 +49,50 @@ describe('Board component', () => {
     });
     state = newTurn(state, 'orange');  // Move to next orange turn so Attack Bot can move.
     state = gameReducer(state, actions.setSelectedTile('1,0,-1'));
-    const gridProps = getHexGridProps({game: state});
 
-    expect(gridProps.hexColors).toEqual({
-      '-4,0,4': 'blue',  // Blue core
-      '0,0,0': 'red',  // Blue piece (can be attacked)
-      '-1,0,1': 'blue',  // Blue piece (blocked from being attacked by the piece at 0,0,0)
-      '4,0,-4': 'orange',  // Orange core
-      '1,0,-1': 'bright_orange',  // Orange piece (currently selected)
-      '2,0,-2': 'bright_orange',  // Orange piece
-      // There are 18 hexes within 2 distance of an interior hex, but 4 are blocked off by the pieces are 2,0,-2 and 0,0,0,
-      // so there should be 14 green hexes.
-      '1,-1,0': 'green',
-      '1,1,-2': 'green',
-      '0,1,-1': 'green',
-      '2,-1,-1': 'green',
-      '1,-2,1': 'green',
-      '2,-2,0': 'green',
-      '0,-1,1': 'green',
-      '1,2,-3': 'green',
-      '0,2,-2': 'green',
-      '2,1,-3': 'green',
-      '-1,2,-1': 'green',
-      '-1,1,0': 'green',
-      '3,-2,-1': 'green',
-      '3,-1,-2': 'green'
+    let dispatchedAction = null;
+    const hexGrid = getComponent(HexGrid, {game: state}, (action => { dispatchedAction = action; }));
+
+    it('are colored as expected', () => {
+      expect(hexGrid.props.hexColors).toEqual({
+        '-4,0,4': 'blue',  // Blue core
+        '0,0,0': 'red',  // Blue piece (can be attacked)
+        '-1,0,1': 'blue',  // Blue piece (blocked from being attacked by the piece at 0,0,0)
+        '4,0,-4': 'orange',  // Orange core
+        '1,0,-1': 'bright_orange',  // Orange piece (currently selected)
+        '2,0,-2': 'bright_orange',  // Orange piece
+        // There are 18 hexes within 2 distance of an interior hex, but 4 are blocked off by the pieces are 2,0,-2 and 0,0,0,
+        // so there should be 14 green hexes.
+        '1,-1,0': 'green',
+        '1,1,-2': 'green',
+        '0,1,-1': 'green',
+        '2,-1,-1': 'green',
+        '1,-2,1': 'green',
+        '2,-2,0': 'green',
+        '0,-1,1': 'green',
+        '1,2,-3': 'green',
+        '0,2,-2': 'green',
+        '2,1,-3': 'green',
+        '-1,2,-1': 'green',
+        '-1,1,0': 'green',
+        '3,-2,-1': 'green',
+        '3,-1,-2': 'green'
+      });
+    });
+
+    it('propagate actions corresponding to color', () => {
+      hexGrid.props.hexagons.forEach(hex => {
+        const color = hexGrid.props.hexColors[HexUtils.getID(hex)];
+        hexGrid.props.actions.onClick(hex);
+
+        if (color == 'green') {
+          expect(dispatchedAction.type).toEqual(actions.MOVE_ROBOT);
+        } else if (color == 'red') {
+          expect(dispatchedAction.type).toEqual(actions.ATTACK);
+        } else {
+          expect(dispatchedAction.type).toEqual(actions.SET_SELECTED_TILE);
+        }
+      });
     });
   });
 

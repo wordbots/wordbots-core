9e71448cb4c936d08c3d400cd01baec6245cc540
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2017-03-08 19:50:30 -0800

    implemented collection filtering by card type

--- src/common/containers/Collection.js
+++ src/common/containers/Collection.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 import { connect } from 'react-redux';
+import Paper from 'material-ui/lib/paper';
+import Toggle from 'material-ui/lib/toggle';
 
 import Card from '../components/game/Card';
 
@@ -14,48 +16,135 @@ function mapDispatchToProps(dispatch) {
   return {};
 }
 
-class CardCreator extends Component {
+class Collection extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      filters: {
+        robots: true,
+        events: true,
+        structures: true
+      }
+    };
+  }
+
+  filterCards(card) {
+    if (!this.state.filters.robots && card.type === 0) {
+      return false;
+    }
+
+    if (!this.state.filters.events && card.type === 1) {
+      return false;
+    }
+
+    if (!this.state.filters.structures && card.type === 3) {
+      return false;
+    }
+
+    return true;
   }
 
   render() {
+    const toggleStyle = {
+      marginBottom: 10
+    };
+
     return (
       <div style={{paddingLeft: 256, /*paddingRight: 256,*/ paddingTop: 64, height: '100%'}}>
         <Helmet title="Collection"/>
 
         <div style={{
           display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'flex-start',
-          maxWidth: '90%',
-          margin: '50px auto'
+          justifyContent: 'space-between'
         }}>
-          {
-            this.props.cards.map(card =>
-              <Card
-                key={card.id}
-                visible
-                name={card.name}
-                spriteID={card.spriteID}
-                type={card.type}
-                text={card.text || ''}
-                stats={card.stats}
-                cardStats={card.stats}
-                cost={card.cost}
-                baseCost={card.cost}
-                scale={1}
-                />
-            )
-          }
+          <Paper style={{
+            margin: 50,
+            padding: 20,
+            width: '18%'
+          }}>
+            <div style={{
+              fontWeight: 100,
+              fontSize: 28,
+              marginBottom: 20
+            }}>Filters</div>
+
+            <div>
+              <div style={{
+                fontWeight: 700,
+                fontSize: 14,
+                marginBottom: 10
+              }}>Card Types</div>
+
+              <Toggle
+                style={toggleStyle}
+                label="Robots"
+                defaultToggled
+                onToggle={(event, toggled) => this.setState({
+                  filters: {
+                    robots: toggled,
+                    events: this.state.filters.events,
+                    structures: this.state.filters.structures
+                  }
+                })}/>
+              <Toggle
+                style={toggleStyle}
+                label="Events"
+                defaultToggled
+                onToggle={(event, toggled) => this.setState({
+                  filters: {
+                    robots: this.state.filters.robots,
+                    events: toggled,
+                    structures: this.state.filters.structures
+                  }
+                })}/>
+              <Toggle
+                style={toggleStyle}
+                label="Structures"
+                defaultToggled
+                onToggle={(event, toggled) => this.setState({
+                  filters: {
+                    robots: this.state.filters.robots,
+                    events: this.state.filters.events,
+                    structures: toggled
+                  }
+                })}/>
+            </div>
+          </Paper>
+
+          <div style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'flex-start',
+            width: '70%',
+            margin: '50px auto'
+          }}>
+            {
+              this.props.cards.filter(this.filterCards.bind(this)).map(card =>
+                <Card
+                  key={card.id}
+                  visible
+                  name={card.name}
+                  spriteID={card.spriteID}
+                  type={card.type}
+                  text={card.text || ''}
+                  stats={card.stats}
+                  cardStats={card.stats}
+                  cost={card.cost}
+                  baseCost={card.cost}
+                  scale={1}
+                  />
+              )
+            }
+          </div>
         </div>
       </div>
     );
   }
 }
 
-CardCreator.propTypes = {
+Collection.propTypes = {
   cards: React.PropTypes.array
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(CardCreator);
+export default connect(mapStateToProps, mapDispatchToProps)(Collection);

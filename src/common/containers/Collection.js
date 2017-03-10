666966a6df38583b08a8566219078b35fe6f21cc
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-03-10 12:48:40 -0800

    cleanup of Collection

--- src/common/containers/Collection.js
+++ src/common/containers/Collection.js
@@ -82,26 +82,182 @@ class Collection extends Component {
     return true;
   }
 
-  render() {
+  renderCard(card) {
+    return (
+      <Card
+        key={card.id}
+        visible
+        name={card.name}
+        spriteID={card.spriteID}
+        type={card.type}
+        text={card.text || ''}
+        stats={card.stats}
+        cardStats={card.stats}
+        cost={card.cost}
+        baseCost={card.cost}
+        source={card.source}
+        scale={1}
+        selected={this.state.selectedCards.includes(card.id)}
+        onCardClick={e => {
+          if (card.source !== 'builtin') {
+            this.setState(state => {
+              if (state.selectedCards.includes(card.id)) {
+                return Object.assign({}, state, {selectedCards: _.without(state.selectedCards, card.id)});
+              } else {
+                return Object.assign({}, state, {selectedCards: [...state.selectedCards, card.id]});
+              }
+            });
+          }
+        }} />
+    );
+  }
+
+  renderDeleteButton() {
+    if (this.state.selectedCards.length > 0) {
+      return (
+        <RaisedButton
+          label="Delete Selected"
+          labelPosition="before"
+          secondary
+          icon={<FontIcon className="material-icons">delete</FontIcon>}
+          style={{width: 300, margin: '0 50px'}}
+          onClick={() => { this.props.onRemoveFromCollection(this.state.selectedCards); }}
+        />
+      );
+    }
+  }
+
+  renderSortControls() {
+    return (
+      <div style={{
+        marginBottom: 20
+      }}>
+        <div style={{
+          fontWeight: 700,
+          fontSize: 14
+        }}>Sorting</div>
+
+        <SelectField
+          style={{
+            width: '100%'
+          }}
+          value={this.state.sortingCriteria}
+          floatingLabelText="Criteria"
+          onChange={(e, i, value) => {
+            this.setState({
+              filters: this.state.filters,
+              manaRange: this.state.manaRange,
+              sortingCriteria: value,
+              sortingOrder: this.state.sortingOrder
+            });
+          }}>
+          <MenuItem value={0} primaryText="By Cost"/>
+          <MenuItem value={1} primaryText="By Name"/>
+          <MenuItem value={2} primaryText="By Type"/>
+          <MenuItem value={3} primaryText="By Creator"/>
+        </SelectField>
+        <SelectField
+          style={{
+            width: '100%'
+          }}
+          value={this.state.sortingOrder}
+          floatingLabelText="Order"
+          onChange={(e, i, value) => {
+            this.setState({
+              filters: this.state.filters,
+              manaRange: this.state.manaRange,
+              sortingCriteria: this.state.sortingCriteria,
+              sortingOrder: value
+            });
+          }}>
+          <MenuItem value={0} primaryText="Ascending"/>
+          <MenuItem value={1} primaryText="Descending"/>
+        </SelectField>
+      </div>
+    );
+  }
+
+  renderFilterControls() {
     const toggleStyle = {
       marginBottom: 10
     };
 
-    function deleteButton() {
-      if (this.state.selectedCards.length > 0) {
-        return (
-          <RaisedButton
-            label="Delete Selected"
-            labelPosition="before"
-            secondary
-            icon={<FontIcon className="material-icons">delete</FontIcon>}
-            style={{width: 300, margin: '0 50px'}}
-            onClick={() => { this.props.onRemoveFromCollection(this.state.selectedCards); }}
-          />
-        );
-      }
-    }
+    return [
+      <div style={{
+        marginBottom: 20
+      }}>
+        <div style={{
+          fontWeight: 700,
+          fontSize: 14,
+          marginBottom: 10
+        }}>Card Types</div>
 
+        <Toggle
+          style={toggleStyle}
+          label="Robots"
+          defaultToggled
+          onToggle={(e, toggled) => {
+            this.setState(s =>
+              Object.assign({}, s,
+                {filters: Object.assign({}, s.filters, {robots: toggled})}
+              )
+            );
+          }} />
+        <Toggle
+          style={toggleStyle}
+          label="Events"
+          defaultToggled
+          onToggle={(e, toggled) => {
+            this.setState(s =>
+              Object.assign({}, s,
+                {filters: Object.assign({}, s.filters, {events: toggled})}
+              )
+            );
+          }} />
+        <Toggle
+          style={toggleStyle}
+          label="Structures"
+          defaultToggled
+          onToggle={(e, toggled) => {
+            this.setState(s =>
+              Object.assign({}, s,
+                {filters: Object.assign({}, s.filters, {structures: toggled})}
+              )
+            );
+          }} />
+      </div>,
+
+      <div style={{
+        marginBottom: 20
+      }}>
+        <div style={{
+          fontWeight: 700,
+          fontSize: 14,
+          marginBottom: 20
+        }}>Card Cost</div>
+
+        <div>
+          <Range
+            step={1}
+            allowCross={false}
+            min={0}
+            max={20}
+            marks={{
+              0: 0,
+              5: 5,
+              10: 10,
+              15: 15,
+              20: 20
+            }}
+            defaultValue={[0, 20]}
+            onChange={values => { this.setState(s => Object.assign({}, s, {manaRange: values})); }}
+          />
+        </div>
+      </div>
+    ];
+  }
+
+  render() {
     return (
       <div style={{paddingLeft: 256, /*paddingRight: 256,*/ paddingTop: 64, height: '100%'}}>
         <Helmet title="Collection"/>
@@ -112,7 +268,7 @@ class Collection extends Component {
           alignItems: 'flex-start'
         }}>
           <div style={{marginTop: 50}}>
-            {deleteButton.bind(this)()}
+            {this.renderDeleteButton()}
 
             <div style={{
               display: 'flex',
@@ -126,33 +282,7 @@ class Collection extends Component {
                 this.props.cards
                   .filter(this.filterCards.bind(this))
                   .sort(this.sortCards.bind(this))
-                  .map(card =>
-                    <Card
-                      key={card.id}
-                      visible
-                      name={card.name}
-                      spriteID={card.spriteID}
-                      type={card.type}
-                      text={card.text || ''}
-                      stats={card.stats}
-                      cardStats={card.stats}
-                      cost={card.cost}
-                      baseCost={card.cost}
-                      source={card.source}
-                      scale={1}
-                      selected={this.state.selectedCards.includes(card.id)}
-                      onCardClick={e => {
-                        if (card.source !== 'builtin') {
-                          this.setState(state => {
-                            if (state.selectedCards.includes(card.id)) {
-                              return Object.assign({}, state, {selectedCards: _.without(state.selectedCards, card.id)});
-                            } else {
-                              return Object.assign({}, state, {selectedCards: [...state.selectedCards, card.id]});
-                            }
-                          });
-                        }
-                      }} />
-                )
+                  .map(this.renderCard.bind(this))
               }
             </div>
           </div>
@@ -169,137 +299,9 @@ class Collection extends Component {
               marginBottom: 20
             }}>Filters</div>
 
-            <div style={{
-              marginBottom: 20
-            }}>
-              <div style={{
-                fontWeight: 700,
-                fontSize: 14
-              }}>Sorting</div>
+            {this.renderSortControls()}
 
-              <SelectField
-                style={{
-                  width: '100%'
-                }}
-                value={this.state.sortingCriteria}
-                floatingLabelText="Criteria"
-                onChange={(e, i, value) => {
-                  this.setState({
-                    filters: this.state.filters,
-                    manaRange: this.state.manaRange,
-                    sortingCriteria: value,
-                    sortingOrder: this.state.sortingOrder
-                  });
-                }}>
-                <MenuItem value={0} primaryText="By Cost"/>
-                <MenuItem value={1} primaryText="By Name"/>
-                <MenuItem value={2} primaryText="By Type"/>
-                <MenuItem value={3} primaryText="By Creator"/>
-              </SelectField>
-              <SelectField
-                style={{
-                  width: '100%'
-                }}
-                value={this.state.sortingOrder}
-                floatingLabelText="Order"
-                onChange={(e, i, value) => {
-                  this.setState({
-                    filters: this.state.filters,
-                    manaRange: this.state.manaRange,
-                    sortingCriteria: this.state.sortingCriteria,
-                    sortingOrder: value
-                  });
-                }}>
-                <MenuItem value={0} primaryText="Ascending"/>
-                <MenuItem value={1} primaryText="Descending"/>
-              </SelectField>
-            </div>
-
-            <div style={{
-              marginBottom: 20
-            }}>
-              <div style={{
-                fontWeight: 700,
-                fontSize: 14,
-                marginBottom: 10
-              }}>Card Types</div>
-
-              <Toggle
-                style={toggleStyle}
-                label="Robots"
-                defaultToggled
-                onToggle={(event, toggled) => this.setState({
-                  filters: {
-                    robots: toggled,
-                    events: this.state.filters.events,
-                    structures: this.state.filters.structures
-                  },
-                  manaRange: this.state.manaRange,
-                  sortingCriteria: this.state.sortingCriteria,
-                  sortingOrder: this.state.sortingOrder
-                })}/>
-              <Toggle
-                style={toggleStyle}
-                label="Events"
-                defaultToggled
-                onToggle={(event, toggled) => this.setState({
-                  filters: {
-                    robots: this.state.filters.robots,
-                    events: toggled,
-                    structures: this.state.filters.structures
-                  },
-                  manaRange: this.state.manaRange,
-                  sortingCriteria: this.state.sortingCriteria,
-                  sortingOrder: this.state.sortingOrder
-                })}/>
-              <Toggle
-                style={toggleStyle}
-                label="Structures"
-                defaultToggled
-                onToggle={(event, toggled) => this.setState({
-                  filters: {
-                    robots: this.state.filters.robots,
-                    events: this.state.filters.events,
-                    structures: toggled
-                  },
-                  manaRange: this.state.manaRange,
-                  sortingCriteria: this.state.sortingCriteria,
-                  sortingOrder: this.state.sortingOrder
-                })}/>
-            </div>
-
-            <div style={{
-              marginBottom: 20
-            }}>
-              <div style={{
-                fontWeight: 700,
-                fontSize: 14,
-                marginBottom: 20
-              }}>Card Cost</div>
-
-              <div>
-                <Range
-                  step={1}
-                  allowCross={false}
-                  min={0}
-                  max={20}
-                  marks={{
-                    0: 0,
-                    5: 5,
-                    10: 10,
-                    15: 15,
-                    20: 20
-                  }}
-                  defaultValue={[0, 20]}
-                  onChange={(values) => this.setState({
-                    filters: this.state.filters,
-                    manaRange: values,
-                    sortingCriteria: this.state.sortingCriteria,
-                    sortingOrder: this.state.sortingOrder
-                  })
-                }/>
-              </div>
-            </div>
+            {this.renderFilterControls()}
           </Paper>
         </div>
       </div>

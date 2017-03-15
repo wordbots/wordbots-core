7d9111b9685d64af415c507e2f634022df44b8f0
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-03-15 00:35:33 -0700

    Set energy cap at 10

--- src/common/reducers/handlers/game/turns.js
+++ src/common/reducers/handlers/game/turns.js
@@ -6,7 +6,7 @@ export function startNewGame(state, decks) {
 
 export function startTurn(state) {
   const player = currentPlayer(state);
-  player.energy.total += 1;
+  player.energy.total = Math.min(player.energy.total + 1, 10);
   player.energy.available = player.energy.total;
   player.robotsOnBoard = _.mapValues(player.robotsOnBoard, (robot =>
     Object.assign({}, robot, {movesLeft: robot.stats.speed})

0b7f5eca8fedb5eef297daa9f9d808369ddf4ec7
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-03-30 16:31:10 -0700

    fix CI

--- src/common/components/Home.js
+++ src/common/components/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Helmet from 'react-helmet';
 
 import Collection from '../containers/Collection';
 
--- src/common/routes.js
+++ src/common/routes.js
@@ -1,4 +1,4 @@
-import { Route, DefaultRoute } from 'react-router';
+import { Route } from 'react-router';
 import React from 'react';
 
 import App from './containers/App';

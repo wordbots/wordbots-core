6935260e3fe0a3251b799cc1aab139f9414f83dc
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-04-12 16:39:53 -0700

    minor fix

--- src/server/socket.js
+++ src/server/socket.js
@@ -1,4 +1,5 @@
 import WebSocket from 'ws';
+import { values } from 'lodash';
 
 import { id as generateID } from '../common/util/common';
 
@@ -88,7 +89,7 @@ export default function launchWebsocketServer(server, path) {
   }
 
   function sendMessage(type, payload = {}, recipientIDs = null) {
-    const sockets = recipientIDs ? recipientIDs.map(id => state.connections[id]) : Object.values(state.connections);
+    const sockets = recipientIDs ? recipientIDs.map(id => state.connections[id]) : values(state.connections);
     const message = JSON.stringify({type, payload});
 
     sockets.forEach(socket => {

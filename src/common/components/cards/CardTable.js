41a4daf79a2bd4f1a62b0c5cd9e1d8c2baf7a341
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2017-04-26 15:23:20 -0700

    implemented pagination for CardTable view

--- src/common/components/cards/CardTable.js
+++ src/common/components/cards/CardTable.js
@@ -7,6 +7,8 @@ import Badge from 'material-ui/Badge';
 import { typeToString } from '../../constants';
 import CardStat from '../card/CardStat';
 
+import PageSwitcher from './PageSwitcher';
+
 export default class CardTable extends Component {
   static propTypes = {
     cards: array,
@@ -18,6 +20,14 @@ export default class CardTable extends Component {
 
     onSelection: func
   };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      page: 1
+    };
+  }
 
   sourceToString(source) {
     if (source === 'user') {
@@ -65,48 +75,56 @@ export default class CardTable extends Component {
   }
 
   render() {
+    const firstCardOnPage = (this.state.page - 1) * 20;
+    const cards = this.props.cards.slice(firstCardOnPage, firstCardOnPage + 20);
+    const maxPages = Math.floor(this.props.cards.length / 20) + 1;
+
     return (
-      <div style={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'flex-start',
-        width: 'calc(100% - 20px)',
-        marginRight: 10,
-        marginBottom: 20,
-        boxShadow: 'rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px'
-      }}>
-        <Table
-          multiSelectable
-          onRowSelection={(selectedRows) => {
-            if (selectedRows === 'none') {
-              this.props.onSelection([]);
-            } else {
-              this.props.onSelection(selectedRows.map(rowIndex => this.props.cards[rowIndex].id));
-            }
-          }}>
-          <TableHeader
-            adjustForCheckbox={false}
-            displaySelectAll={false}
-            enableSelectAll={false}
-            style={{backgroundColor: '#f44336'}}>
-            <TableRow>
-              <TableHeaderColumn width={100} style={{color: 'white'}}>Name</TableHeaderColumn>
-              <TableHeaderColumn width={100} style={{color: 'white'}}>Type</TableHeaderColumn>
-              <TableHeaderColumn width={50} style={{color: 'white'}}>Creator</TableHeaderColumn>
-              <TableHeaderColumn style={{color: 'white'}}>Card Text</TableHeaderColumn>
-              <TableHeaderColumn width={30} style={{color: 'white'}}>Attack</TableHeaderColumn>
-              <TableHeaderColumn width={30} style={{color: 'white'}}>Health</TableHeaderColumn>
-              <TableHeaderColumn width={30} style={{color: 'white'}}>Speed</TableHeaderColumn>
-              <TableHeaderColumn width={30} style={{color: 'white'}}>Cost</TableHeaderColumn>
-            </TableRow>
-          </TableHeader>
-          <TableBody
-            displayRowCheckbox={false}
-            deselectOnClickaway={false}
-            showRowHover>
-              {this.props.cards.map(this.renderCardRow.bind(this))}
-          </TableBody>
-        </Table>
+      <div>
+        <div style={{
+          width: 'calc(100% - 20px)',
+          marginRight: 10,
+          marginBottom: 20,
+          boxShadow: 'rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px'
+        }}>
+          <Table
+            multiSelectable
+            onRowSelection={(selectedRows) => {
+              if (selectedRows === 'none') {
+                this.props.onSelection([]);
+              } else {
+                this.props.onSelection(selectedRows.map(rowIndex => cards[rowIndex].id));
+              }
+            }}>
+            <TableHeader
+              adjustForCheckbox={false}
+              displaySelectAll={false}
+              enableSelectAll={false}
+              style={{backgroundColor: '#f44336'}}>
+              <TableRow>
+                <TableHeaderColumn width={100} style={{color: 'white'}}>Name</TableHeaderColumn>
+                <TableHeaderColumn width={100} style={{color: 'white'}}>Type</TableHeaderColumn>
+                <TableHeaderColumn width={50} style={{color: 'white'}}>Creator</TableHeaderColumn>
+                <TableHeaderColumn style={{color: 'white'}}>Card Text</TableHeaderColumn>
+                <TableHeaderColumn width={30} style={{color: 'white'}}>Attack</TableHeaderColumn>
+                <TableHeaderColumn width={30} style={{color: 'white'}}>Health</TableHeaderColumn>
+                <TableHeaderColumn width={30} style={{color: 'white'}}>Speed</TableHeaderColumn>
+                <TableHeaderColumn width={30} style={{color: 'white'}}>Cost</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody
+              displayRowCheckbox={false}
+              deselectOnClickaway={false}
+              showRowHover>
+                {cards.map(this.renderCardRow.bind(this))}
+            </TableBody>
+          </Table>
+        </div>
+        <PageSwitcher
+          page={this.state.page}
+          maxPages={maxPages}
+          prevPage={() => this.setState({page: this.state.page - 1})}
+          nextPage={() => this.setState({page: this.state.page + 1})}/>
       </div>
     );
   }

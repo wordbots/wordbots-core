dd0a38b8daa5e7e256792ea9843fc001f7bc3250
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2017-05-13 13:40:45 -0700

    started refactoring Play page

--- src/common/components/multiplayer/Lobby.js
+++ src/common/components/multiplayer/Lobby.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 import { array, func, object } from 'prop-types';
-import { shuffle } from 'lodash';
 
-import { KEEP_DECKS_UNSHUFFLED } from '../../constants';
-import { instantiateCard, cardsInDeck } from '../../util/cards';
-
-import DeckPicker from './DeckPicker';
-import GameBrowser from './GameBrowser';
-import HostGame from './HostGame';
+import CustomLobby from './CustomLobby';
 import LobbyStatus from './LobbyStatus';
 import Waiting from './Waiting';
 
@@ -31,16 +25,6 @@ export default class Lobby extends Component {
     };
   }
 
-  get hasNoDecks() {
-    return this.props.availableDecks.length === 0;
-  }
-
-  get deck() {
-    const deck = this.props.availableDecks[this.state.selectedDeck];
-    const cards = cardsInDeck(deck, this.props.cards).map(instantiateCard);
-    return KEEP_DECKS_UNSHUFFLED ? cards : shuffle(cards);
-  }
-
   render() {
     const skt = this.props.socket;
 
@@ -53,25 +37,14 @@ export default class Lobby extends Component {
           usernameMap={skt.clientIdToUsername}
           onConnect={this.props.onConnect} />
         {
-          skt.hosting ?
-            <Waiting /> :
-            <div>
-              <DeckPicker
-                cards={this.props.cards}
-                availableDecks={this.props.availableDecks}
-                selectedDeckIdx={this.state.selectedDeck}
-                onChooseDeck={idx => { this.setState({selectedDeck: idx}); }} />
-              <GameBrowser
-                cannotJoinGame={this.hasNoDecks}
-                openGames={skt.waitingPlayers}
-                inProgressGames={skt.games}
-                usernameMap={skt.clientIdToUsername}
-                onJoinGame={(gameId, gameName) => { this.props.onJoinGame(gameId, gameName, this.deck); }}
-                onSpectateGame={(gameId, gameName) => { this.props.onSpectateGame(gameId, gameName); }} />
-              <HostGame
-                disabled={this.hasNoDecks}
-                onHostGame={gameName => { this.props.onHostGame(gameName, this.deck); }} />
-            </div>
+          skt.hosting ? 
+            <Waiting /> : 
+            <CustomLobby 
+              availableDecks={this.props.availableDecks}
+              cards={this.props.cards}
+              onJoinGame={this.props.onJoinGame}
+              onSpectateGame={this.props.onSpectateGame}
+              onHostGame={this.props.onHostGame} />
         }
       </div>
     );

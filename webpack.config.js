87c793409a683c5aaca056107cf657966ff9f3ec
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-05-15 14:08:27 -0700

    as above

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,25 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 /* eslint-enable import/order */
 
+const BABEL_LOADER_DEV_OPTIONS = {
+  presets: ['es2015', 'stage-2', 'react'],
+  plugins: [
+    ['react-transform', {transforms: [
+      {
+        transform: 'react-transform-hmr',
+        imports: ['react'],
+        locals:  ['module']
+      },
+      {
+        transform: 'react-transform-catch-errors',
+        imports: ['react','redbox-react']
+      }
+    ]}],
+    'transform-decorators-legacy',
+    'transform-class-properties'
+  ]
+};
+
 let webpackConfig = {
   entry: [
     'babel-polyfill',
@@ -14,6 +33,14 @@ let webpackConfig = {
   ],
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: process.env.NODE_ENV === 'production' ? {} : BABEL_LOADER_DEV_OPTIONS
+        }
+      },
       { test: /\.(png|jpg|gif|jpeg)$/, loader: 'url-loader?limit=8192'},
       { test: /\.css$/, loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader?sourceMap'}) },
       { test: /\.(eot|svg|ttf|woff|woff2)$/, loader: 'file-loader?name=public/fonts/[name].[ext]' }
@@ -44,22 +71,9 @@ if (process.env.NODE_ENV === 'production') {
       'webpack-hot-middleware/client',
       ...webpackConfig.entry
     ],
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/,
-          include: __dirname
-        },
-        ...webpackConfig.module.rules
-      ]
-    },
     plugins: [
       new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production')
-        }
+        'process.env': { NODE_ENV: JSON.stringify('production') }
       }),
       new webpack.optimize.UglifyJsPlugin({sourceMap: true}),
       ...webpackConfig.plugins
@@ -71,36 +85,6 @@ if (process.env.NODE_ENV === 'production') {
 } else {
   webpackConfig = merge(webpackConfig, {
     devtool: 'inline-source-map',
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['es2015', 'stage-2', 'react'],
-              plugins: [
-                ['react-transform', {transforms: [
-                  {
-                    transform: 'react-transform-hmr',
-                    imports: ['react'],
-                    locals:  ['module']
-                  },
-                  {
-                    transform: 'react-transform-catch-errors',
-                    imports: ['react','redbox-react']
-                  }
-                ]}],
-                'transform-decorators-legacy',
-                'transform-class-properties'
-              ]
-            }
-          }
-        },
-        ...webpackConfig.module.rules
-      ]
-    },
     plugins: [
       new webpack.HotModuleReplacementPlugin(),
       ...webpackConfig.plugins

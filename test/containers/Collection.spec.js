b4ccf11900c443bd1ae513ed85ffce18dcae3e4f
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-05-18 21:47:29 -0700

    Add some more tests to Collection.spec

--- test/containers/Collection.spec.js
+++ test/containers/Collection.spec.js
@@ -6,6 +6,7 @@ import CardGrid from '../../src/common/components/cards/CardGrid';
 import CardTable from '../../src/common/components/cards/CardTable';
 import FilterControls from '../../src/common/components/cards/FilterControls';
 import LayoutControls from '../../src/common/components/cards/LayoutControls';
+import PageSwitcher from '../../src/common/components/cards/PageSwitcher';
 import SearchControls from '../../src/common/components/cards/SearchControls';
 import collectionReducer from '../../src/common/reducers/collection';
 import creatorReducer from '../../src/common/reducers/creator';
@@ -16,11 +17,18 @@ describe('Collection container', () => {
   const CARDS_IN_PAGE = 20;
   const DEFAULT_COST_MAX = 20;
 
-  it('should be able to open the card collection and use the sidebar controls', () => {
-    const dispatchedActions = [];
-    let state = combineState();
+  let state, dispatch, collection;
 
-    function dispatch(action) {
+  function component(componentClass) {
+    const components = scryRenderedComponentsWithType(collection, componentClass);
+    return (components && components.length === 1) ? components[0] : components;
+  }
+
+  beforeEach(() => {
+    const dispatchedActions = [];
+
+    state = combineState();
+    dispatch = (action) => {
       // console.log(action);
       dispatchedActions.push(action);
       state = {  // Mimic the behavior of the combined reducers.
@@ -29,23 +37,31 @@ describe('Collection container', () => {
         game: gameReducer(state.game, action),
         global: state.global
       };
-    }
+    };
 
-    const collection = renderElement(createCollection(state, dispatch), true);
+    collection = renderElement(createCollection(state, dispatch), true);
+  });
 
-    function component(componentClass) {
-      const components = scryRenderedComponentsWithType(collection, componentClass);
-      return (components && components.length === 1) ? components[0] : null;
-    }
+  it('should be able to open the card collection', () => {
+    expect(component(CardGrid).props.cards.length).toEqual(CARDS_IN_PAGE);
+    expect(component(CardTable).length).toEqual(0);
+  });
+
+  it('should be able to use the sidebar controls', () => {
+    // Pagination controls
+
+    component(PageSwitcher)[0].props.nextPage();
+    // TODO Test that the page has changed.
+    component(PageSwitcher)[0].props.prevPage();
 
     // Layout controls
 
     expect(component(CardGrid).props.cards.length).toEqual(CARDS_IN_PAGE);
-    expect(component(CardTable)).toBeNull();
+    expect(component(CardTable).length).toEqual(0);
 
     component(LayoutControls).props.onSetLayout(1);
 
-    expect(component(CardGrid)).toBeNull();
+    expect(component(CardGrid).length).toEqual(0);
     expect(component(CardTable).props.cards.length).toEqual(CARDS_IN_PAGE);
 
     component(LayoutControls).props.onSetLayout(0);
@@ -70,5 +86,24 @@ describe('Collection container', () => {
     expect(component(CardGrid).props.cards.length).toEqual(numCardsInRange);
 
     component(FilterControls).props.onSetCostRange([0, DEFAULT_COST_MAX]);
+  });
+
+  it('should be able to select custom cards but not built-in cards', () => {
+    state.collection.cards.push({
+      id: 'customCardId',
+      name: 'Custom Card',
+      source: 'user',
+      type: 1,
+      cost: 1,
+      text: ''
+    });
+
+    collection = renderElement(createCollection(state, dispatch), true);
+
+    component(CardGrid).props.onCardClick(cards[0].id);
+    expect(collection.state.selectedCardIds.length).toEqual(0);
+
+    component(CardGrid).props.onCardClick('customCardId');
+    expect(collection.state.selectedCardIds.length).toEqual(1);
   });
 });

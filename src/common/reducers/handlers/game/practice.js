7fbfcc16ed4d22e4d89327fd2bcc8a2027fee5ba
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-06-20 19:07:49 -0700

    AI can now play random cards

--- src/common/reducers/handlers/game/practice.js
+++ src/common/reducers/handlers/game/practice.js
@@ -1,8 +1,12 @@
-import { cloneDeep } from 'lodash';
+import { cloneDeep, findIndex, sample } from 'lodash';
 
-import { passTurn } from '../../../util/game';
+import { TYPE_EVENT } from '../../../constants';
+import { validPlacementHexes, passTurn } from '../../../util/game';
 import { collection } from '../../../store/cards';
 import defaultState from '../../../store/defaultGameState';
+import HexUtils from '../../../components/react-hexgrid/HexUtils';
+
+import { setSelectedCard, placeCard } from './cards';
 
 export function startPractice(state) {
   // Reset game state and enable tutorial mode.
@@ -26,7 +30,41 @@ export function aiResponse(state) {
     return state;
   }
 
-  const rand = Math.random();
-  console.log(rand);
-  return rand < 0.2 ? passTurn(state, state.currentTurn) : state;
+  const rnd = Math.random();
+
+  if (rnd < 0.05) {
+    return passTurn(state, state.currentTurn);
+  } else if (rnd < 0.50) {
+    return tryToPlayCard(state);
+  } else {
+    return tryToMoveRobot(state);
+  }
+}
+
+function tryToPlayCard(state) {
+  const ai = state.players[state.currentTurn];
+  const playableCards = ai.hand.filter(c => c.cost <= ai.energy.available);
+
+  if (playableCards.length > 0) {
+    const card = sample(playableCards);
+    const idx = findIndex(ai.hand, {id: card.id});
+
+    state = setSelectedCard(state, ai.name, idx);
+
+    if (card.type === TYPE_EVENT) {
+      // Play event by "clicking twice".
+      state = setSelectedCard(state, ai.name, idx);
+    } else {
+      const validHexes = validPlacementHexes(state, ai.name, card.type);
+      if (validHexes.length > 0) {
+        state = placeCard(state, idx, HexUtils.getID(sample(validHexes)));
+      }
+    }
+  }
+
+  return state;
+}
+
+function tryToMoveRobot(state) {
+  return state;
 }

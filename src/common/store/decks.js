6fdfe72c3511008b95bc9e7b29faa16dd71ce9a7
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-10-01 04:11:55 -0700

    RoboRampage built-in deck (#479)

---
+++ src/common/store/decks.js
@@ -0,0 +1,38 @@
+import * as cards from './cards';
+
+/* eslint-disable import/prefer-default-export */
+export const aggro = [
+  // Robots (22)
+  cards.speedyBotCard,
+  cards.speedyBotCard,
+  cards.dojoDiscipleCard,
+  cards.dojoDiscipleCard,
+  cards.mercenaryBlacksmithCard,
+  cards.mercenaryBlacksmithCard,
+  cards.bloodSwordmasterCard,
+  cards.bloodSwordmasterCard,
+  cards.roboSlugCard,
+  cards.roboSlugCard,
+  cards.martyrBotCard,
+  cards.recklessBerserkerCard,
+  cards.recklessBerserkerCard,
+  cards.kernelEaterCard,
+  cards.kernelEaterCard,
+  cards.hermesCard,
+  cards.recruiterBotCard,
+  cards.energyHoarderCard,
+  cards.energyHoarderCard,
+  cards.leapFrogBotCard,
+  cards.leapFrogBotCard,
+  cards.generalBotCard,
+
+  // Events (8)
+  cards.gustOfWindCard,
+  cards.gustOfWindCard,
+  cards.rampageCard,
+  cards.rampageCard,
+  cards.concentrationCard,
+  cards.concentrationCard,
+  cards.shockCard,
+  cards.shockCard
+];

d50ecc86435575c128df355bb217f45bdb161056
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-10-22 16:04:25 -0700

    Fix test warnings

--- test/beforeTest.js
+++ test/beforeTest.js
@@ -1,3 +1,8 @@
 /* eslint-disable no-console */
 console.warn = jest.genMockFn();
 /* eslint-enable no-console */
+
+// See https://reactjs.org/docs/javascript-environment-requirements.html
+global.requestAnimationFrame = (callback) => {
+  setTimeout(callback, 0);
+};

084d4e3f3e676d31d8665a1b837ce396e1fd134b
Alex Nisnevich <alex.nisnevich@gmail.com>
2017-10-21 18:45:50 -0700

    Factor out RecentCardsCarousel; de-duplicate cards in it and disallow builtin cards (#493)
    * Factor out RecentCardsCarousel; de-duplicate cards in it and disallow builtin cards
    
    * minor
    
    * oops
    
    * minor

---
+++ src/common/components/cards/RecentCardsCarousel.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import Carousel from 'react-slick';
+import { uniqBy } from 'lodash';
+
+import { listenToRecentCards } from '../../util/firebase';
+import { builtinCardNames } from '../../store/cards';
+import Card from '../card/Card';
+
+export default class RecentCardsCarousel extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      recentCards: []
+    };
+  }
+
+  componentDidMount() {
+    listenToRecentCards(data => {
+      const recentCards = uniqBy(Object.values(data), 'name')
+                            .filter(card => !builtinCardNames.includes(card.name))
+                            .reverse()
+                            .slice(0, 10);
+      this.setState({ recentCards });
+    });
+  }
+
+  render() {
+    if (this.state.recentCards.length > 0) {
+      return (
+        <div>
+          <Carousel dots infinite autoplay pauseOnHover
+            arrows={false}
+            speed={500}
+            autoplaySpeed={1500}
+            slidesToScroll={1}
+            responsive={[
+              {breakpoint: 950, settings: {slidesToShow: 3}},
+              {breakpoint: 1150, settings: {slidesToShow: 4}},
+              {breakpoint: 1350, settings: {slidesToShow: 5}},
+              {breakpoint: 1550, settings: {slidesToShow: 6}},
+              {breakpoint: 1750, settings: {slidesToShow: 7}},
+              {breakpoint: 1950, settings: {slidesToShow: 8}},
+              {breakpoint: 100000, settings: {slidesToShow: 8}}
+            ]}
+          >
+            {
+              this.state.recentCards.map(card =>
+                <div key={card.id} style={{
+                  display: 'flex',
+                  justifyContent: 'center'
+                }}>
+                  {Card.fromObj(card)}
+                </div>
+              )
+            }
+          </Carousel>
+          <p style={{
+            marginTop: 30,
+            color: '#999',
+            fontSize: 20,
+            fontWeight: 'bold',
+            textTransform: 'uppercase',
+            textAlign: 'center'
+          }}>
+            Most recently created cards
+          </p>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}

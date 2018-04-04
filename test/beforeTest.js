21027cff1032f9c24298abc0ce138e0bce1df388
Alex Nisnevich <alex.nisnevich@gmail.com>
2018-04-03 22:38:16 -0700

    minor

--- test/beforeTest.js
+++ test/beforeTest.js
@@ -1,10 +1,6 @@
 import Enzyme from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
-/* eslint-disable no-console */
-// console.warn = jest.genMockFn();
-/* eslint-enable no-console */
-
 // See https://reactjs.org/docs/javascript-environment-requirements.html
 global.requestAnimationFrame = (callback) => {
   setTimeout(callback, 0);

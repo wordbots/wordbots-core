21769efba697707dd1882a7a8062801656dd6f27
Alex Nisnevich <alex.nisnevich@gmail.com>
2018-04-21 19:34:02 -0700

    Multiplayer refactor part I

---
+++ src/server/multiplayer/MultiplayerServerState.js
@@ -0,0 +1,162 @@
+/* eslint-disable no-console */
+import { compact, pull, reject } from 'lodash';
+
+import { id as generateID } from '../../common/util/common';
+
+// Returns a copy of a game with the given client
+// removed from both the players and spectators lists (if present),
+// or null if this results in the game no longer having >=2 active players.
+function withoutClient(game, clientID) {
+  const updatedGame = Object.assign({}, game, {
+    players: game.players.filter(p => p !== clientID),
+    spectators: game.players.filter(p => p !== clientID)
+  });
+  return updatedGame.players.length >= 2 ? updatedGame : null;
+}
+
+export default class MultiplayerServerState {
+  state = {
+    connections: {},  // map of { clientID: websocket }
+    games: [],  // array of { id, name, players, playerColors, spectators, actions, decks, usernames, startingSeed }
+    waitingPlayers: [],  // array of { id, name, deck, players }
+    playersOnline: [],  // array of clientIDs
+    usernames: {}  // map of { clientID: username }
+  };
+
+  /* Getters */
+
+  // Returns a serializable subset of the state for broadcast as an INFO message.
+  serialize() {
+    const { games, waitingPlayers, playersOnline, usernames } = this.state;
+    return { games, waitingPlayers, playersOnline, usernames };
+  }
+
+  // Returns the socket corresponding to a given player.
+  getClientSocket(clientID) {
+    return this.state.connections[clientID];
+  }
+
+  // Returns all websocket connections corresponding to the given clientIDs,
+  // or ALL connections if no clientIDs are specified.
+  getClientSockets(clientIDs = null) {
+    return clientIDs ? clientIDs.map(this.getClientSocket) : Object.values(this.state.connections);
+  }
+
+  // Returns the username to use for the given player.
+  getClientUsername(clientID) {
+    return this.state.usernames[clientID] || clientID;
+  }
+
+  // Returns the game that the given player is in, if any.
+  lookupGameByClient(clientID) {
+    return this.state.games.find(g => g.players.includes(clientID));
+  }
+
+  // Returns all *other* players in the game that the given player is in, if any.
+  getAllOpponents(clientID) {
+    const game = this.state.games.find(g => g.players.includes(clientID) || g.spectators.includes(clientID));
+    if (game) {
+      return [...game.players, ...game.spectators].filter(id => id !== clientID);
+    }
+  }
+
+  // Returns all *other* players currently in the lobby.
+  getAllOtherPlayersInLobby(clientID) {
+    const inGamePlayerIds = this.state.games.reduce((acc, game) => acc.concat(game.players), []);
+    return this.state.playersOnline.filter(id => id !== clientID && !inGamePlayerIds.includes(id));
+  }
+
+  /* Mutations */
+
+  // Connect a player at the specified websocket to the server.
+  connectClient(clientID, socket) {
+    this.state.connections[clientID] = socket;
+    this.state.playersOnline.push(clientID);
+    console.log(`${clientID} joined the room.`);
+  }
+
+  // Disconnect a player from the server.
+  // Return the game that the player was in (if any).
+  disconnectClient(clientID) {
+    pull(this.state.playersOnline, clientID);
+    this.state.waitingPlayers = reject(this.state.waitingPlayers, { id: clientID });
+
+    const game = this.state.lookupGameByClient(clientID);
+    if (game) {
+      this.leaveGame(clientID);
+    }
+
+    delete this.state.connections[clientID];
+    console.log(`${this.getClientUsername(clientID)} left the game.`);
+
+    return game;
+  }
+
+  // Set a player's username.
+  setClientUsername(clientID, newUsername) {
+    this.state.usernames[clientID] = newUsername;
+  }
+
+  // Add an action to a player's game.
+  appendGameAction(clientID, action) {
+    const game = this.state.games.find(g => g.players.includes(clientID));
+    if (game) {
+      game.actions.push(action);
+    }
+  }
+
+  // Make a player host a game with the given game name and using the given deck.
+  hostGame(clientID, name, deck) {
+    this.state.waitingPlayers.push({
+      id: clientID,
+      players: [clientID],
+      name,
+      deck
+    });
+    console.log(`${clientID} started game ${name}.`);
+  }
+
+  // Make a player join the given opponent's hosted game with the given deck.
+  // Returns the game joined.
+  joinGame(clientID, opponentID, deck) {
+    const opponent = this.state.waitingPlayers.find(p => p.id === opponentID);
+    const game = {
+      id: opponentID,
+      name: opponent.name,
+
+      players: [clientID, opponentID],
+      playerColors: {[clientID]: 'blue', [opponentID]: 'orange'},
+      spectators: [],
+
+      actions: [],
+      decks: {orange: opponent.deck, blue: deck},
+      usernames: {
+        orange: this.getClientUsername(opponentID),
+        blue: this.getClientUsername(clientID)
+      },
+      startingSeed: generateID()
+    };
+
+    this.state.waitingPlayers = reject(this.state.waitingPlayers, { id: opponentID });
+    this.state.games.push(game);
+
+    console.log(`${clientID} joined game ${game.name} against ${opponentID}.`);
+    return game;
+  }
+
+  // Make a player join the given game as a spectator.
+  // Returns the game joined, or null if the game wasn't found.
+  spectateGame(clientID, gameID) {
+    const game = find(this.state.games, { id: gameID });
+    if (game) {
+      game.spectators.push(clientID);
+      console.log(`${clientID} joined game ${game.name} as a spectator.`);
+      return game;
+    }
+  }
+
+  // Remove a player from any game that they are currently in.
+  leaveGame(clientID) {
+    this.state.games = compact(this.state.games.map(withoutClient));
+  }
+}

bb9270452dc983098be3d18b4b57ac2950229ad8
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2018-05-15 22:29:48 -0700

    feat(): add @material-ui/core and update Status

--- src/common/components/game/Status.js
+++ src/common/components/game/Status.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import { string } from 'prop-types';
-import Snackbar from 'material-ui/Snackbar';
+import { string, object } from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Snackbar from '@material-ui/core/Snackbar';
 
-export default class Status extends Component {
+const styles = {
+  root: {
+    zIndex: 2000,
+    transform: 'translateX(calc(-50% - 128px))'
+  },
+  message: {
+    textTransform: 'uppercase',
+    fontWeight: 600,
+    fontSize: 16
+  }
+};
+
+class Status extends Component {
   static propTypes = {
     type: string,
-    message: string
+    message: string,
+    classes: object
   };
 
   get color() {
@@ -16,28 +30,22 @@ export default class Status extends Component {
   }
 
   render() {
-    // TODO When we switch over to material-ui@next, there will be actual support
-    // for top-center placement of snackbars.
-    // For now we do this ugly hack: https://github.com/mui-org/material-ui/issues/4641
+    const { message, classes } = this.props;
+
     return (
       <Snackbar
-        open
-        message={this.props.message}
-        style={{
-          top: 0,
-          bottom: 'auto',
-          left: (window.innerWidth - 350) / 2,
-          transform: this.props.message ? 'translate3d(0, 0, 0)' : 'translate3d(0, -50px, 0)'
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'center'
         }}
-        bodyStyle={{
-          minWidth: 320
+        classes={{
+          root: classes.root
         }}
-        contentStyle={{
-          color: this.color,
-          textAlign: 'center',
-          transition: 'none'
-        }}
+        open={message}
+        message={<span style={{ color: this.color }} className={classes.message}>{message}</span>}
       />
     );
   }
 }
+
+export default withStyles(styles)(Status);

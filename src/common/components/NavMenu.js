dc3e335308a7d376851421c26dea701ff4dbb035
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2018-07-07 16:29:46 -0700

    feat(): remove singleplayer components and create multiplayer files

--- src/common/components/NavMenu.js
+++ src/common/components/NavMenu.js
@@ -5,6 +5,7 @@ import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import FontIcon from 'material-ui/FontIcon';
 
+import { SIDEBAR_WIDTH, SIDEBAR_COLLAPSED_WIDTH } from '../constants';
 import { isFlagSet, toggleFlag, zeroWidthJoin } from '../util/browser';
 import Tooltip from '../components/Tooltip';
 
@@ -80,7 +81,7 @@ export default class NavMenu extends React.Component {
       containerStyle={{
         top: 54,
         paddingTop: 10,
-        width: this.isExpanded ? 224 : 64,
+        width: this.isExpanded ? SIDEBAR_WIDTH : SIDEBAR_COLLAPSED_WIDTH,
         transition: 'width 200ms ease-in-out',
         height: 'calc(100% - 54px)',
         overflow: 'visible'
---
+++ src/common/components/multiplayer/MultiplayerModeSelection.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { func } from 'prop-types';
+
+import GameMode from './GameMode';
+
+export default class MultiplayerModeSelection extends React.Component {
+  static propTypes = {
+    onSelectMode: func
+  };
+
+  handleClickCasual = () => {
+    this.props.onSelectMode('casual');
+  }
+
+  handleClickUnrankedMatchmaking = () => {
+    this.props.onSelectMode('unranked');
+  }
+
+  handleClickRankedMatchmaking = () => {
+    this.props.onSelectMode('ranked');
+  }
+
+  render() {
+    return (
+      <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+      }}>
+        <GameMode
+          name="Casual Game"
+          imagePath="/static/casual.png"
+          modesPerRow={3}
+          onSelect={this.handleClickCasual} />
+        <GameMode
+          name="Unranked Matchmaking"
+          imagePath="/static/casual.png"
+          modesPerRow={3}
+          onSelect={this.handleClickUnrankedMatchmaking} />
+        <GameMode
+          name="Ranked Matchmaking"
+          imagePath="/static/casual.png"
+          modesPerRow={3}
+          onSelect={this.handleClickRankedMatchmaking} />
+      </div>
+    );
+  }
+}

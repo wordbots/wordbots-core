138e9273f1f711bf7d6e32210ab86f1cd6bda51d
Alex Nisnevich <alex.nisnevich@gmail.com>
2018-09-23 12:53:46 -0700

    minor

--- src/common/guards.ts
+++ src/common/guards.ts
@@ -1,14 +1,16 @@
 // Type guards for intersection types.
 // See https://www.typescriptlang.org/docs/handbook/advanced-types.html
 
+import { isNil } from 'lodash';
+
 import * as w from './types';
 
 export function isObject(target: w.Targetable): target is w.Object {
-  return (target as w.Object).card !== undefined;
+  return !isNil(target) && (target as w.Object).card !== undefined;
 }
 
 export function isPlayerState(target: w.Targetable): target is w.PlayerInGameState {
-  return (target as w.PlayerInGameState).robotsOnBoard !== undefined;
+  return !isNil(target) && (target as w.PlayerInGameState).robotsOnBoard !== undefined;
 }
 
 export function isCardObfuscated(target: w.PossiblyObfuscatedCard): target is w.ObfuscatedCard {

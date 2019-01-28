0f093c7e4049f6209dd0e5d6432534a7e8b58540
Alex Nisnevich <alex.nisnevich@gmail.com>
2019-01-27 21:41:30 -0800

    CardGrid.tsx, CardTable.tsx, PageSwitcher.tsx

---
+++ src/common/components/cards/CardGrid.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+
+import * as w from '../../types';
+import { id } from '../../util/common';
+import { inBrowser } from '../../util/browser';
+import { splitSentences } from '../../util/cards';
+import Card from '../card/Card';
+import Sentence from '../card/Sentence';
+
+interface CardGridProps {
+  cards: w.CardInStore[]
+  selectedCardIds: string[]
+  selectable: boolean
+  onCardClick: (cardId: w.CardId) => void
+}
+
+export default class CardGrid extends React.Component<CardGridProps> {
+  public render(): JSX.Element {
+    return (
+      <div style={{
+        width: 'calc(100% - 40px)',
+        margin: '0 20px'
+      }}>
+        <div style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          justifyContent: 'flex-start'
+        }}>{!inBrowser() ? null : this.props.cards.map(this.renderCard)}</div>
+      </div>
+    );
+  }
+
+  private renderCard = (card: w.CardInStore) => (
+    <div
+      key={card.id || id()}
+      style={{
+        marginRight: 15,
+        marginTop: -12
+    }}>
+      <Card
+        collection
+        id={card.id}
+        name={card.name}
+        spriteID={card.spriteID}
+        spriteV={card.spriteV}
+        type={card.type}
+        text={splitSentences(card.text).map((text) => Sentence(text))}
+        rawText={card.text || ''}
+        stats={card.stats}
+        cardStats={card.stats}
+        cost={card.cost}
+        baseCost={card.cost}
+        source={card.source}
+        selected={this.props.selectable && this.props.selectedCardIds.includes(card.id)}
+        onCardClick={this.props.onCardClick} />
+    </div>
+  )
+}
---
+++ src/common/components/cards/CardTable.tsx
@@ -0,0 +1,117 @@
+import * as React from 'react';
+import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
+import Badge from 'material-ui/Badge';
+
+import * as w from '../../types';
+import { typeToString } from '../../constants';
+import { id } from '../../util/common';
+import CardStat from '../card/CardStat';
+
+interface CardGridProps {
+  cards: w.CardInStore[]
+  selectedCardIds: string[]
+  selectable: boolean
+  onCardClick: (cardId: w.CardId) => void
+}
+
+export default class CardTable extends React.Component<CardGridProps> {
+  public render(): JSX.Element {
+    return (
+      <div>
+        <div style={{
+          width: 'calc(100% - 20px)',
+          marginRight: 10,
+          marginBottom: 20,
+          boxShadow: 'rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px'
+        }}>
+          <Table
+            className="cardTable"
+            multiSelectable
+            selectable={this.props.selectable}
+            onCellClick={this.handleCellClick}
+            style={{minWidth: 650}}
+          >
+            <TableHeader
+              adjustForCheckbox={false}
+              displaySelectAll={false}
+              enableSelectAll={false}
+            >
+              <TableRow>
+                <TableHeaderColumn style={{ width: 130}}>Name</TableHeaderColumn>
+                <TableHeaderColumn style={{ width: 50}}>Type</TableHeaderColumn>
+                <TableHeaderColumn style={{ width: 50}}>Creator</TableHeaderColumn>
+                <TableHeaderColumn>Text</TableHeaderColumn>
+                <TableHeaderColumn style={{ width: 30}}>Attack</TableHeaderColumn>
+                <TableHeaderColumn style={{ width: 30}}>Health</TableHeaderColumn>
+                <TableHeaderColumn style={{ width: 30}}>Speed</TableHeaderColumn>
+                <TableHeaderColumn style={{ width: 30}}>Cost</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody
+              displayRowCheckbox={false}
+              deselectOnClickaway={false}
+              showRowHover>
+                {this.props.cards.map(this.renderCardRow)}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    );
+  }
+
+  private sourceToString(source?: string): string {
+    if (source === 'user') {
+      return 'You';
+    } else {
+      return 'Built-In';
+    }
+  }
+
+  private handleCellClick = (row: number) => {
+    this.props.onCardClick(this.props.cards[row].id);
+  }
+
+  private renderCardRowStat(type: w.Attribute, stats: w.CardInStore['stats']): JSX.Element | string {
+    if (stats && stats[type]) {
+      return (
+        <CardStat
+          noTooltip
+          type={type}
+          base={stats ? stats[type] : ''}
+          current={stats ? stats[type] : ''}
+          scale={1} />
+      );
+    } else {
+      return '';
+    }
+  }
+
+  private renderCardCost(cost: number): JSX.Element {
+    return (
+      <Badge
+        badgeContent={cost}
+        badgeStyle={{
+          width: 30, height: 30, backgroundColor: '#00bcd4',
+          fontFamily: 'Carter One', fontSize: 16, color: 'white'
+        }}
+        style={{padding: 0, width: 24, height: 24}} />
+    );
+  }
+
+  private renderCardRow = (card: w.CardInStore) => (
+    <TableRow
+      key={card.id || id()}
+      selected={this.props.selectable && this.props.selectedCardIds.includes(card.id)}
+      selectable={!this.props.selectable || card.source !== 'builtin'}
+    >
+      <TableRowColumn style={{width: 130}}>{card.name}</TableRowColumn>
+      <TableRowColumn style={{width: 50}}>{typeToString(card.type)}</TableRowColumn>
+      <TableRowColumn style={{width: 50}}>{this.sourceToString(card.source)}</TableRowColumn>
+      <TableRowColumn>{card.text}</TableRowColumn>
+      <TableRowColumn style={{width: 30, textAlign: 'center'}}>{this.renderCardRowStat('attack', card.stats)}</TableRowColumn>
+      <TableRowColumn style={{width: 30, textAlign: 'center'}}>{this.renderCardRowStat('health', card.stats)}</TableRowColumn>
+      <TableRowColumn style={{width: 30, textAlign: 'center'}}>{this.renderCardRowStat('speed', card.stats)}</TableRowColumn>
+      <TableRowColumn style={{width: 30, textAlign: 'center'}}>{this.renderCardCost(card.cost)}</TableRowColumn>
+    </TableRow>
+  )
+}

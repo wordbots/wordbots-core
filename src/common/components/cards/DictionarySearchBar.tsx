31200fbf3bd6e8e7372e9056aaf6baa140332cd1
Alex Nisnevich <alex.nisnevich@gmail.com>
2019-05-09 23:34:56 -0700

    Persist dictionary search bar text

--- src/common/components/cards/DictionarySearchBar.tsx
+++ src/common/components/cards/DictionarySearchBar.tsx
@@ -4,23 +4,16 @@ import TextField from 'material-ui/TextField';
 import * as React from 'react';
 
 interface DictionarySearchBarProps {
+  text: string
   onChange: (searchText: string) => void
 }
 
-interface DictionarySearchBarState {
-  text: string
-}
-
-export default class DictionarySearchBar extends React.Component<DictionarySearchBarProps, DictionarySearchBarState> {
-  public state = {
-    text: ''
-  };
-
+export default class DictionarySearchBar extends React.Component<DictionarySearchBarProps> {
   public render(): JSX.Element {
     return (
       <div style={{display: 'flex', alignItems: 'center', width: '100%', height: 56}}>
         <TextField
-          value={this.state.text}
+          value={this.props.text}
           hintText="Search for a term ... "
           style={{width: '100%', margin: '0 10px'}}
           hintStyle={{color: '#AAA'}}
@@ -32,17 +25,11 @@ export default class DictionarySearchBar extends React.Component<DictionarySearc
     );
   }
 
-  private setText = (text: string) => {
-    this.setState({ text }, () => {
-      this.props.onChange(text);
-    });
-  }
-
-  private handleChangeText = (_e: React.FormEvent<{}>, value: string) => { this.setText(value); };
-  private handleClickClear = () => { this.setText(''); };
+  private handleChangeText = (_e: React.FormEvent<{}>, value: string) => { this.props.onChange(value); };
+  private handleClickClear = () => { this.props.onChange(''); };
 
   private renderClearButton(): React.ReactNode {
-    if (this.state.text !== '') {
+    if (this.props.text !== '') {
       return (
         <IconButton
           style={{width: '20%'}}

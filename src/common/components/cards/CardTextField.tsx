efe23484cbe4849e3ea3bfcb4c1943e473734a0d
Alex Nisnevich <alex.nisnevich@gmail.com>
2019-05-12 12:54:44 -0700

    Contract keywords for Did You Mean suggestions (#1098)
    * Contract keywords for Did You Mean suggestions
    
    * minor

--- src/common/components/cards/CardTextField.tsx
+++ src/common/components/cards/CardTextField.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { BigramProbs } from 'word-ngrams';
 
 import * as w from '../../types';
+import { contractKeywords } from '../../util/cards';
 import { bigramNLL } from '../../util/language';
 
 import CardTextSuggestion from './CardTextSuggestion';
@@ -21,9 +22,12 @@ export default class CardTextField extends React.Component<CardTextFieldProps> {
     const { bigramProbs, sentences } = this.props;
     if (bigramProbs) {
       return _(sentences)
-              .flatMap((s) =>
+              .flatMap((s: w.Sentence) =>
                 (s.result.suggestions || []).map((suggestion) =>
-                  ({ original: s.sentence.trim(), suggestion })
+                  ({
+                    original: s.sentence.trim(),
+                    suggestion: contractKeywords(suggestion)
+                  })
                 )
               )
               .sortBy(({ suggestion }) => bigramNLL(suggestion, bigramProbs))

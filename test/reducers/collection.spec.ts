f8191dc0fe3096bd440c1321d66e18a0055a1fa1
Alex Nisnevich <alex.nisnevich@gmail.com>
2019-10-12 12:56:28 -0700

    minor

--- test/reducers/collection.spec.ts
+++ test/reducers/collection.spec.ts
@@ -1,15 +1,20 @@
 import { difference, noop, omit } from 'lodash';
 
-import { duplicateCard } from '../../src/common/actions/collection';
+import * as collectionActions from '../../src/common/actions/collection';
 import { saveCard } from '../../src/common/actions/creator';
 import { TYPE_ROBOT } from '../../src/common/constants';
 import collection from '../../src/common/reducers/collection';
+import { oneBotCard, twoBotCard } from '../../src/common/store/cards';
 import defaultState from '../../src/common/store/defaultCollectionState';
 import * as w from '../../src/common/types';
 import { createCardFromProps } from '../../src/common/util/cards';
 import * as firebase from '../../src/common/util/firebase';
 
 jest.mock('../../src/common/util/firebase');
+
+function args(mockedFn: any, callNum: number = 0): any[] {
+  return mockedFn.mock.calls[callNum];
+}
 
 function expectCardsToBeEqual(card1: w.CardInStore, card2: w.CardInStore): void {
   expect(omit(card1, ['id', 'metadata'])).toEqual(omit(card2, ['id', 'metadata']));
@@ -18,7 +23,8 @@ function expectCardsToBeEqual(card1: w.CardInStore, card2: w.CardInStore): void 
 function defaultCollectionState(): w.CollectionState {
   return {
     ...defaultState,
-    cards: [...defaultState.cards]
+    cards: [...defaultState.cards],
+    decks: [...defaultState.decks]
   };
 }
 
@@ -26,6 +32,7 @@ describe('Collection reducer', () => {
   beforeAll(() => {
     (firebase.lookupCurrentUser as any).mockImplementation(() => ({ uid: 'test-user-id', displayName: 'test-user-name' }));
     (firebase.saveCard as any).mockImplementation(noop);
+    (firebase.saveDeck as any).mockImplementation(noop);
   });
 
   afterEach(() => {
@@ -34,8 +41,7 @@ describe('Collection reducer', () => {
 
   it('DUPLICATE_CARD', () => {
     let state: w.CollectionState = defaultCollectionState();
-    const oneBotCard: w.CardInStore = state.cards.find((c) => c.name === 'One Bot')!;
-    state = collection(state, duplicateCard(oneBotCard));
+    state = collection(state, collectionActions.duplicateCard(oneBotCard));
 
     const newCards = difference(state.cards, defaultState.cards);
     expect(newCards.length).toEqual(1);
@@ -88,7 +94,38 @@ describe('Collection reducer', () => {
 
       // firebase.saveCard() should have been called twice (once for the creation, once for the edit)
       expect(firebase.saveCard).toHaveBeenCalledTimes(2);
-      expectCardsToBeEqual((firebase.saveCard as any).mock.calls[1][0], { ...testBotCard, cost: 2 });
+      expectCardsToBeEqual(args(firebase.saveCard, 1)[0], { ...testBotCard, cost: 2 });
+    });
+  });
+
+  describe('SAVE_DECK', () => {
+    it('creates new deck', () => {
+      let state: w.CollectionState = defaultCollectionState();
+      state = collection(state, collectionActions.saveDeck(null, 'One Bot Deck', [oneBotCard.id], null));
+
+      expect(state.decks.length).toEqual(defaultState.decks.length + 1);
+      expect(state.decks.map((d) => d.name)).toContain('One Bot Deck');
+      expect(state.decks.find((d) => d.name === 'One Bot Deck')!.cardIds === [oneBotCard.id]);
+      expect(firebase.saveDeck).toHaveBeenCalledTimes(1);
+      expect(args(firebase.saveDeck)[0]).toMatchObject({ authorId: 'test-user-id', name: 'One Bot Deck', cardIds: [oneBotCard.id], setId: null });
+    });
+
+    it('edits existing deck', () => {
+      let state: w.CollectionState = defaultCollectionState();
+      // Create One Bot Deck with one card ...
+      state = collection(state, collectionActions.saveDeck(null, 'One Bot Deck', [oneBotCard.id], null));
+      // ... then add another card to it
+      const oneBotDeck: w.DeckInStore = state.decks.find((d) => d.name === 'One Bot Deck')!;
+      state = collection(state, collectionActions.saveDeck(oneBotDeck.id, 'One Bot Deck', [oneBotCard.id, twoBotCard.id], null));
+
+      // Only the edited deck should be in the player's collection now
+      expect(state.decks.length).toEqual(defaultState.decks.length + 1);
+      expect(state.decks.map((d) => d.name)).toContain('One Bot Deck');
+      expect(state.decks.find((d) => d.name === 'One Bot Deck')!.cardIds === [oneBotCard.id, twoBotCard.id]);
+
+      // firebase.saveDeck() should have been called twice (once for the creation, once for the edit)
+      expect(firebase.saveDeck).toHaveBeenCalledTimes(2);
+      expect(args(firebase.saveDeck, 1)[0]).toMatchObject({ authorId: 'test-user-id', name: 'One Bot Deck', cardIds: [oneBotCard.id, twoBotCard.id], setId: null });
     });
   });
 });

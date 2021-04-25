154f2cb0885e0a8616d3fcfe951a74fcde78b49e
Alex Nisnevich <alex.nisnevich@gmail.com>
2021-04-25 16:55:36 -0700

    Conveyor belt UI in RecentCardsCarousel (fix #1420) (#1442)
    * Conveyor belt UI in RecentCardsCarousel (fix #1420)
    
    * closer together
    
    * little lighter
    
    * minor

--- src/common/components/cards/RecentCardsCarousel.tsx
+++ src/common/components/cards/RecentCardsCarousel.tsx
@@ -1,11 +1,13 @@
 import { History } from 'history';
 import { range } from 'lodash';
 import * as React from 'react';
+import Measure, { ContentRect } from 'react-measure';
 import Carousel, { ResponsiveObject } from 'react-slick';
 
 import * as w from '../../types';
 import { mostRecentCards } from '../../util/firebase';
 import { Card } from '../card/Card';
+import ConveyorBelt from '../ConveyorBelt';
 
 import CardProvenanceDescription from './CardProvenanceDescription';
 
@@ -16,19 +18,23 @@ interface RecentCardsCarouselProps {
 
 interface RecentCardsCarouselState {
   recentCards: w.CardInStore[]
+  paused: boolean
+  width?: number
 }
 
 export default class RecentCardsCarousel extends React.Component<RecentCardsCarouselProps, RecentCardsCarouselState> {
   public static MAX_CARDS_TO_SHOW = 8;
 
-  public state = {
-    recentCards: []
+  public state: RecentCardsCarouselState = {
+    recentCards: [],
+    paused: false
   };
 
   public constructor(props: RecentCardsCarouselProps) {
     super(props);
     this.initializeCarousel(props.userId);
   }
+
 
   get carouselBreakpoints(): ResponsiveObject[] {
     const BASE_WIDTH_BREAKPOINT = 950;
@@ -41,45 +47,27 @@ export default class RecentCardsCarousel extends React.Component<RecentCardsCaro
     }));
   }
 
-  public render(): JSX.Element | null {
-    const { recentCards } = this.state;
+  public render(): JSX.Element {
+    return (
+      <Measure offset onResize={this.handleResize}>
+        {({ measureRef }) => (
+          <div ref={measureRef}>
+            {this.renderInner()}
+          </div>
+        )}
+      </Measure>
+    );
+  }
+
+  private renderInner(): JSX.Element | null {
+    const { recentCards, paused, width } = this.state;
 
     if (recentCards.length > 0) {
       return (
         <div>
-          <Carousel
-            dots
-            infinite
-            autoplay
-            pauseOnHover
-            arrows={false}
-            draggable={false}
-            speed={500}
-            autoplaySpeed={1500}
-            slidesToScroll={1}
-            responsive={[
-              ...this.carouselBreakpoints,
-              {breakpoint: 100000, settings: {slidesToShow: RecentCardsCarousel.MAX_CARDS_TO_SHOW}}
-            ]}
-          >
-            {
-              recentCards.map((card, idx) =>
-                <div
-                  key={idx}
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'center'
-                  }}
-                >
-                  {Card.fromObj(card, { onCardClick: () => { this.handleClickCard(card); }})}
-                  <CardProvenanceDescription card={card} style={{ fontSize: 11, color: '#888', maxWidth: 155 }} />
-                </div>
-              )
-            }
-          </Carousel>
-          <p
+          <div
             style={{
-              marginTop: 30,
+              marginTop: 10,
               color: '#999',
               fontSize: 20,
               fontWeight: 'bold',
@@ -88,7 +76,49 @@ export default class RecentCardsCarousel extends React.Component<RecentCardsCaro
             }}
           >
             Most recently created cards
-          </p>
+          </div>
+          <div
+            className="recentCardsCarousel"
+            onMouseOver={this.handleMouseOver}
+            onMouseLeave={this.handleMouseLeave}
+          >
+            <Carousel
+              dots
+              infinite
+              autoplay
+              pauseOnHover
+              arrows={false}
+              draggable={false}
+              speed={500}
+              autoplaySpeed={1500}
+              slidesToScroll={1}
+              responsive={[
+                ...this.carouselBreakpoints,
+                {breakpoint: 100000, settings: {slidesToShow: RecentCardsCarousel.MAX_CARDS_TO_SHOW}}
+              ]}
+            >
+              {
+                recentCards.map((card, idx) =>
+                  <div
+                    key={idx}
+                    style={{
+                      display: 'flex',
+                      justifyContent: 'center'
+                    }}
+                  >
+                    {Card.fromObj(card, { onCardClick: () => { this.handleClickCard(card); }})}
+                    <CardProvenanceDescription card={card} style={{ position: 'relative', top: 22, fontSize: 11, color: '#888', maxWidth: 155 }} />
+                  </div>
+                )
+              }
+            </Carousel>
+          </div>
+          <div style={{ position: 'relative', top: -53 }}>
+            <ConveyorBelt
+              width={width || 0}
+              paused={paused}
+            />
+          </div>
         </div>
       );
     } else {
@@ -110,5 +140,12 @@ export default class RecentCardsCarousel extends React.Component<RecentCardsCaro
 
   private handleClickCard = (card: w.CardInStore) => {
     this.props.history.push(`/card/${card.id}`, { card });
+  }
+
+  private handleMouseOver = () => { this.setState({ paused: true }); }
+  private handleMouseLeave = () => { this.setState({ paused: false }); }
+
+  private handleResize = (contentRect: ContentRect) => {
+    this.setState({ width: contentRect.offset?.width });
   }
 }

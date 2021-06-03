2a605ebe5ab8cbac7999f9c7304dc8fb12c6dda7
Alex Nisnevich <alex.nisnevich@gmail.com>
2021-06-03 00:02:50 -0700

    add ability to see drafted cards

--- src/common/components/game/DraftArea.tsx
+++ src/common/components/game/DraftArea.tsx
@@ -1,10 +1,12 @@
+import { Button, Dialog, DialogActions, DialogContent } from '@material-ui/core';
 import * as React from 'react';
 
-import { BACKGROUND_Z_INDEX, BLUE_PLAYER_COLOR, ORANGE_PLAYER_COLOR } from '../../constants';
+import { BACKGROUND_Z_INDEX, BLUE_PLAYER_COLOR, MAX_Z_INDEX, ORANGE_PLAYER_COLOR } from '../../constants';
 import * as w from '../../types';
 import { opponent } from '../../util/game';
 
 import DraftCardPicker from './DraftCardPicker';
+import DraftDeck from './DraftDeck';
 import ForfeitButton from './ForfeitButton';
 import LeftControls from './LeftControls';
 import PlayerName from './PlayerName';
@@ -21,20 +23,14 @@ interface DraftAreaProps {
   onToggleFullscreen: () => void
 }
 
-export default class DraftArea extends React.Component<DraftAreaProps> {
-  get numCardsYouHaveDrafted(): number | undefined {
-    const { draft, player } = this.props;
-    if (player !== 'neither') {
-      return draft[player].cardsDrafted.length;
-    }
-  }
+interface DraftAreaState {
+  isDeckOpen: boolean
+}
 
-  get numCardsOpponentHasDrafted(): number | undefined {
-    const { draft, player } = this.props;
-    if (player !== 'neither') {
-      return draft[opponent(player)].cardsDrafted.length;
-    }
-  }
+export default class DraftArea extends React.Component<DraftAreaProps, DraftAreaState> {
+  public state = {
+    isDeckOpen: false
+  };
 
   get currentCardGroup(): w.CardInGame[] | null {
     const { draft, player } = this.props;
@@ -50,6 +46,7 @@ export default class DraftArea extends React.Component<DraftAreaProps> {
       player, draft, isGameOver, volume,
       onForfeit, onSetVolume, onToggleFullscreen
     } = this.props;
+    const { isDeckOpen } = this.state;
 
     return (
       <div
@@ -99,6 +96,32 @@ export default class DraftArea extends React.Component<DraftAreaProps> {
             onForfeit={onForfeit}
           />
         </div>
+
+        <Dialog
+          open={isDeckOpen && !isGameOver}
+          PaperProps={{
+            style: {
+              width: 1010,
+              maxWidth: 1010,
+              overflow: 'none'
+            }
+          }}
+          style={{ zIndex: MAX_Z_INDEX }}
+          onClose={this.handleToggleShowDeck}
+        >
+          <DialogContent>
+            <DraftDeck cards={draft[player as 'blue' | 'orange'].cardsDrafted} />
+          </DialogContent>
+          <DialogActions>
+            <Button
+              key="Close"
+              color="primary"
+              onClick={this.handleToggleShowDeck}
+            >
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     );
   }
@@ -153,9 +176,19 @@ export default class DraftArea extends React.Component<DraftAreaProps> {
           fontSize: '1.5em',
           fontFamily: 'VT323',
         }}>
-          {numCardsDrafted}/30 cards
+          {
+            !isOpponent && numCardsDrafted > 0
+              ? <a className="underline" onClick={this.handleToggleShowDeck}>
+                  {numCardsDrafted}/30 cards
+                </a>
+              : <span>{numCardsDrafted}/30 cards</span>
+          }
         </div>
       </React.Fragment>
     );
+  }
+
+  private handleToggleShowDeck = () => {
+    this.setState(({ isDeckOpen }) => ({ isDeckOpen: !isDeckOpen }));
   }
 }

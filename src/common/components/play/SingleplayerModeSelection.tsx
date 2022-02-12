de8a8fc6ea148a8d89dfb6c0d112fc139e341220
Alex Nisnevich <alex.nisnevich@gmail.com>
2022-02-11 16:41:46 -0800

    new game mode art

--- src/common/components/play/SingleplayerModeSelection.tsx
+++ src/common/components/play/SingleplayerModeSelection.tsx
@@ -12,19 +12,19 @@ export default class SinglePlayerModeSelection extends React.Component<SinglePla
       <div>
         <GameMode
           name="Tutorial"
-          imagePath="/static/tutorial.png"
+          imagePath="/static/tutorial-new.png"
           explanation="Follow the interactive tutorial to learn how to play Wordbots."
           onSelect={this.handleClickTutorial}
         />
         <GameMode
           name="Practice"
-          imagePath="/static/practice.png"
+          imagePath="/static/practice-new.png"
           explanation="Pick a deck and play a practice game against the computer."
           onSelect={this.handleClickPractice}
         />
         <GameMode
           name="Sandbox"
-          imagePath="/static/casual.png"
+          imagePath="/static/sandbox-new.png"
           explanation="Try out any card while controlling both players in sandbox mode."
           onSelect={this.handleClickSandbox}
         />

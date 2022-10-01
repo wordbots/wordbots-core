a00dcc004d0f28c757c4948d6b5e3ba2c7fa5fcc
Alex Nisnevich <alex.nisnevich@gmail.com>
2022-09-30 19:22:37 -0700

    util/decks

--- src/common/util/decks.ts
+++ src/common/util/decks.ts
@@ -5,30 +5,34 @@ import * as w from '../types';
 
 import { instantiateCard } from './cards';
 
+/** Given a DeckInStore, a user's pool of cards, and a list of sets,
+  * return an array of the (unshuffled) cards in the deck. */
 export function cardsInDeck(deck: w.DeckInStore, userCards: w.CardInStore[], sets: w.Set[]): w.CardInStore[] {
   const set: w.Set | null = deck.setId && sets.find((s) => s.id === deck.setId) || null;
   const cardPool = set ? set.cards : userCards;
   return compact((deck.cardIds || []).map((id) => cardPool.find((c) => c.id === id)));
 }
 
+/** Given a DeckInStore, a user's pool of cards, and a list of sets,
+  * return a shuffled array of the cards in the deck. */
 export function shuffleCardsInDeck(deck: w.DeckInStore, userCards: w.CardInStore[], sets: w.Set[]): w.CardInGame[] {
   const unshuffledCards = cardsInDeck(deck, userCards, sets);
   const potentiallyShuffledCards = KEEP_DECKS_UNSHUFFLED ? unshuffledCards : shuffle(unshuffledCards);
   return potentiallyShuffledCards.map(instantiateCard);
 }
 
-// "Unpacks" a deck so that it can be used in a game.
-// { cardIds } => { cardIds, cards }
+/** Given a DeckInStore, returns an "unpacked" DeckInGame so that it can be used in a game.
+  * i.e. materializes the cards in the deck: { cardIds } => { cardIds, cards } */
 export function unpackDeck(deck: w.DeckInStore, userCards: w.CardInStore[], sets: w.Set[]): w.DeckInGame {
   return { ...deck, cards: shuffleCardsInDeck(deck, userCards, sets) };
 }
 
-// Note: this method is parametrized because it needs to support both Deck and DeckInStore.
-// eslint-disable-next-line import/prefer-default-export
+/** Sorts an array of decks, by timestamp descending for user decks, followed by built-in decks.
+  * Note: this method is parameterized because it needs to support both Deck and DeckInStore. */
 export function sortDecks<T extends w.DeckInStore>(decks: T[]): T[] {
   const decksWithTimestamps: T[] = decks.map((deck: T) => {
     if (deck.timestamp) {
-      return deck;                      // Deck has a timestamp
+      return deck;  // Deck has a timestamp
     } else if (deck.id[0] === '[') {
       return { ...(deck as w.DeckInStore), timestamp: 0 } as T; // Deck is built-in
     } else {

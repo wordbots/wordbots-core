f218ef6851efb087af30e7bf07c06a978819e9e8
Alex Nisnevich <alex.nisnevich@gmail.com>
2022-10-20 19:36:23 -0700

    Profile page UI/UX improvements

--- src/common/util/multiplayer.ts
+++ src/common/util/multiplayer.ts
@@ -2,10 +2,16 @@
 
 import * as m from '../../server/multiplayer/multiplayer';
 
+const GUEST_PREFIX = 'guest_';
+
 /** Generates a guest user ID given a `ClientID`. */
 export const guestUID = (clientID: m.ClientID): string =>
-  `guest_${clientID}`;
+  `${GUEST_PREFIX}${clientID}`;
 
 /** Generates a guest username given a `ClientID`. */
 export const guestUsername = (clientID: m.ClientID): string =>
   `Guest_${clientID.slice(0, 6)}`;
+
+/** Given a user id, returns whether it belongs to a guest user. */
+export const isGuest = (userId: string): boolean =>
+  userId.startsWith(GUEST_PREFIX);

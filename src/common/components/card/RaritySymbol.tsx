6d4dcec4e99175dc38cf0af9d35268de7a002f5c
Alex Nisnevich <alex.nisnevich@gmail.com>
2023-08-06 18:18:57 -0700

    what if we just package the font we want

--- src/common/components/card/RaritySymbol.tsx
+++ src/common/components/card/RaritySymbol.tsx
@@ -2,7 +2,6 @@ import { capitalize } from 'lodash';
 import * as React from 'react';
 
 import * as w from '../../types';
-import { BROWSER_INFO } from '../../util/browser';
 import Tooltip from '../Tooltip';
 
 interface RaritySymbolProps {
@@ -12,22 +11,14 @@ interface RaritySymbolProps {
 }
 
 export default function RaritySymbol({ rarity, scale, isEditing }: RaritySymbolProps): JSX.Element {
-  const isLinux: boolean = BROWSER_INFO?.os?.toLowerCase() === 'linux';
-
-  // We need to ensure that the symbols are all rendered in the same font, so we can be reasonably sure of rendering them all visually at the same size.
-  // Because some Linux distros don't have Arial by default, we use DejaVu Sans in the font stack and try to ensure that it actually gets used (but ONLY on Linux).
-  // So, below, isLinux is really a proxy for isFontDejaVuSans (unfortunately it's not metrically compatible with Arial/Helvetica, so we have to special-case it ...)
-  // TODO make the whole thing more robust with something like https://developer.mozilla.org/en-US/docs/Web/API/FontFaceSet/check ?
-  const fontFamily = isLinux ? '"DejaVu Sans", sans-serif' : '"Arial Unicode MS", sans-serif';
-
   return (
     <span style={{
       display: 'inline-block',
       userSelect: 'none',
-      fontFamily,
-      fontSize: (isLinux ? 17 : ({ rare: 18, uncommon: 26, common: 26 })[rarity]) * (scale || 1),
-      marginTop: (isLinux ? 5 : ({ rare: 5, uncommon: 3, common: -1 })[rarity]) * (scale || 1),
-      marginLeft: (isLinux ? -1 : ({ rare: -1, uncommon: -2, common: 0 })[rarity]) * (scale || 1),
+      fontFamily: '"DejaVu Sans", sans-serif',
+      fontSize: 17 * (scale || 1),
+      marginTop: 5 * (scale || 1),
+      marginLeft: -1 * (scale || 1),
       color: ({ rare: '#d4af37', uncommon: '#aaa9ad', common: undefined })[rarity]
     }}>
       <Tooltip

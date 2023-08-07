36974418bf9a6c0259a490518d61419cae284b31
Alex Nisnevich <alex.nisnevich@gmail.com>
2023-08-06 17:49:53 -0700

    minor

--- src/common/components/card/RaritySymbol.tsx
+++ src/common/components/card/RaritySymbol.tsx
@@ -2,6 +2,7 @@ import { capitalize } from 'lodash';
 import * as React from 'react';
 
 import * as w from '../../types';
+import { BROWSER_INFO } from '../../util/browser';
 import Tooltip from '../Tooltip';
 
 interface RaritySymbolProps {
@@ -11,14 +12,21 @@ interface RaritySymbolProps {
 }
 
 export default function RaritySymbol({ rarity, scale, isEditing }: RaritySymbolProps): JSX.Element {
+  const isLinux: boolean = BROWSER_INFO?.os?.toLowerCase() === 'linux';
+
+  // We need to ensure that the symbols are all rendered in the same font, so we can be reasonably sure of rendering them all visually at the same size.
+  // Because some Linux distros don't have Arial by default, we use DejaVu Sans in the font stack and try to ensure that it actually gets used (but ONLY on Linux).
+  // So, below, isLinux is really a proxy for isFontDejaVuSans (unfortunately it's not metrically compatible with Arial/Helvetica, so we have to special-case it ...)
+  const fontFamily = isLinux ? '"DejaVu Sans", sans-serif' : 'Arial, Helvetica, sans-serif';
+
   return (
     <span style={{
       display: 'inline-block',
       userSelect: 'none',
-      fontFamily: 'Arial, Helvetica, sans-serif',
-      fontSize: (({ rare: 17, uncommon: 17, common: 26 })[rarity]) * (scale || 1),
-      marginTop: (({ rare: 5, uncommon: 3, common: -1 })[rarity]) * (scale || 1),
-      marginLeft: (({ rare: -1, uncommon: -2, common: 0 })[rarity]) * (scale || 1),
+      fontFamily,
+      fontSize: (isLinux ? 17 : ({ rare: 17, uncommon: 21, common: 26 })[rarity]) * (scale || 1),
+      marginTop: (isLinux ? 5 : ({ rare: 5, uncommon: 3, common: -1 })[rarity]) * (scale || 1),
+      marginLeft: (isLinux ? -1 : ({ rare: -1, uncommon: -2, common: 0 })[rarity]) * (scale || 1),
       color: ({ rare: '#d4af37', uncommon: '#aaa9ad', common: undefined })[rarity]
     }}>
       <Tooltip

3db59551511b60a723732c145056f137663a29dd
Alex Nisnevich <alex.nisnevich@gmail.com>
2023-08-06 18:41:14 -0700

    minor

--- src/common/components/card/RaritySymbol.tsx
+++ src/common/components/card/RaritySymbol.tsx
@@ -15,6 +15,9 @@ export default function RaritySymbol({ rarity, scale, isEditing }: RaritySymbolP
     <span style={{
       display: 'inline-block',
       userSelect: 'none',
+      // DejaVu Sans is one of few fonts that includes all 3 characters we need and scales them fairly similarly.
+      // Having all three characters within one font solves most of our previous issues with having to scale different characters differently.
+      // It's packaged by default with most Linux distros, and we serve DejaVuSans.ttf for other OSs.
       fontFamily: '"DejaVu Sans", sans-serif',
       fontSize: (rarity === 'rare' ? 19 : 17) * (scale || 1),
       marginTop: 5 * (scale || 1),
